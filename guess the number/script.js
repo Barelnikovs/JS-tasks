0f431e0e7b7a28c1c08ab9857f82874ff2431cb7
Barelnikovs <107426665+Barelnikovs@users.noreply.github.com>
2022-11-04 13:19:21 +0300

    "Guess thenumber" is done

--- guess the number/script.js
+++ guess the number/script.js
@@ -8,13 +8,15 @@ let checkNum = document.getElementById('check-number')
 let unknownNum
 let counter = 0
 
-function pastPrompt(num) {
+function pushPrompt(num) {
     let arr = ['Нажмите "Новая игра"',
-        'Загадано целое число от 1 до 100. Попробуйте угадать его за как можно меньшее количество попыток. После каждой попытки будет появляться подсказка',
+        'Загадано целое число от 1 до 100. Попробуйте угадать его за как можно меньшее количество попыток. После каждой проверки будет появляться подсказка.',
         'Число больше',
         'Число меньше',
         'Введённое число не входит в диапозон от 1 до 100',
-        'Введённое не целое число',]
+        'Введённое число не целое',
+        `Победа!
+    Использовано попыток ${counter}`,]
     prompts.textContent = arr[num]
 }
 
@@ -27,6 +29,7 @@ newGame.addEventListener('click', function () {
     changeVisibleBlock()
     unknownNum = Math.ceil(Math.random() * 100)
     console.log(unknownNum)
+    pushPrompt(1)
     enterNum.select()
 })
 
@@ -37,8 +40,22 @@ enterNum.addEventListener('keyup', function (event) {
 })
 
 checkNum.addEventListener('click', function () {
-    console.log('нажатие Enter')
-    //if (enterNum.value >= 1 && enterNum.value < unknownNum && Number.isInteger(enter.value)){
-
-    //}
+    if (enterNum.value >= 1 && enterNum.value < unknownNum && Number.isInteger(Number(enterNum.value))) {
+        pushPrompt(2)
+        ++counter
+    } if (enterNum.value <= 100 && enterNum.value > unknownNum && Number.isInteger(Number(enterNum.value))) {
+        pushPrompt(3)
+        ++counter
+    } if (!Number.isInteger(Number(enterNum.value))) {
+        pushPrompt(5)
+    } if (enterNum.value > 100 || enterNum.value < 1) {
+        pushPrompt(4)
+    } if (Number(enterNum.value) === unknownNum) {
+        ++counter
+        pushPrompt(6)
+        changeVisibleBlock()
+        counter = 0
+    }
+    enterNum.value = ''
+    enterNum.select()
 })
